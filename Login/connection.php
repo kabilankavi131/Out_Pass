c9f21e70e0d6d15c80a57672e3d9632b4c779e44
Kabilankavi141 <121872971+Kabilankavi141@users.noreply.github.com>
2023-05-30 02:49:59 -0400

    Database Connected;

--- Login/connection.php
+++ Login/connection.php
@@ -8,6 +8,6 @@ mysqli_query($con,$query);
 $data="select * from registration";
 $execution=mysqli_query($con,$data);
 if($execution){
-    echo "<center><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><h1 style='color:blue;'>Sign Up Successfully</h1></center>";
+    include 'index.html';
 }
 ?>
---
+++ Login/validate.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+<link rel="stylesheet" type="text/css" href="css/my-login.cs">
+<script>
+    function login(){
+Swal.fire("Log in successfull");
+      }
+      function failure(){
+        Swal.fire("User Not Found :(");
+      }
+</script>
+  <title>Validation</title>
+</head>
+<body>
+<?php
+$email=$_POST['email'];
+$password=$_POST['password'];
+$mysqli = new mysqli("localhost","root","","kitoutpass");
+$name="select  `name` from `registration` where `email`='$email'";
+$result = $mysqli->query($name);
+if(mysqli_num_rows($result)==1){
+    while ($row = $result->fetch_assoc()) {
+        $user= $row['name'];
+        echo '<script type="text/javascript">login();</script>';
+    }
+}
+else{
+     echo '<script type="text/javascript">failure();</script>';
+}
+?>
+</body>
+</html>
